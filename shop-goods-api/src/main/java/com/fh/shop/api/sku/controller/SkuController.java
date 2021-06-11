6064961dd127eee806a2822c72f24b5d58096221
gc <2011766189@qq.com>
2021-06-11 10:32:27 +0800

    aaa

--- shop-goods-api/src/main/java/com/fh/shop/api/sku/controller/SkuController.java
+++ shop-goods-api/src/main/java/com/fh/shop/api/sku/controller/SkuController.java
@@ -2,18 +2,22 @@ package com.fh.shop.api.sku.controller;
 
 import com.fh.shop.api.sku.biz.ISkuService;
 import com.fh.shop.common.ServerResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/api")
+@Api(tags = "商品接口")
 public class SkuController {
 
     @Resource(name = "ISkuService")
     private ISkuService iSkuService;
 
     @GetMapping("/skus")
+    @ApiOperation("查询商品的信息")
     private ServerResponse findSkuList() {
         return iSkuService.findSkuList();
     }
--- shop-zuul/src/main/java/com/fh/shop/ShopZuulServer.java
+++ shop-zuul/src/main/java/com/fh/shop/ShopZuulServer.java
@@ -3,12 +3,57 @@ package com.fh.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ShopZuulServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShopZuulServer.class,args);
+        SpringApplication.run(ShopZuulServer.class, args);
     }
+
+
+    @Component
+    @Primary
+    class SwaggerDocumentationConfig implements SwaggerResourcesProvider {
+        private final RouteLocator routeLocator;
+
+        public SwaggerDocumentationConfig(RouteLocator routeLocator) {
+            this.routeLocator = routeLocator;
+        }
+
+        @Override
+        public List<SwaggerResource> get() {
+            List<SwaggerResource> resources = new ArrayList<>();
+            List<Route> routes = routeLocator.getRoutes();
+            //动态添加方便
+            routes.forEach(route -> {
+            //从各个服务里获取文档
+                resources.add(swaggerResource(route.getId(), route.getFullPath().replace("**", "v2/api-docs"), "1.0"));
+            });
+            //一个一个添加，使用起来麻烦
+//            resources.add(swaggerResource("shop-cate-api", "/shop-cate-api/v2/api-docs", "2.0"));
+//            resources.add(swaggerResource("shop-cart-api", "/shop-cart-api/v2/api-docs", "2.0"));
+            return resources;
+        }
+
+        private SwaggerResource swaggerResource(String name, String location, String version) {
+            SwaggerResource swaggerResource = new SwaggerResource();
+            swaggerResource.setName(name);
+            swaggerResource.setLocation(location);
+            swaggerResource.setSwaggerVersion(version);
+            return swaggerResource;
+        }
+    }
+
+
 }
